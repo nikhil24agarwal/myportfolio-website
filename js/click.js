1bf768d15a6667d69b4505b14b00427975188595
nikhil24agarwal <nikhil12386@yahoo.com>
2020-07-12 14:39:13 +0530

    animation initial completed

--- js/click.js
+++ js/click.js
@@ -1,18 +1,2 @@
 
-              $("document").ready(function(){  $(".myimage").click(function(){
-                $(".skills").css({"transform":"translate(-200px,-200px)","transition-duration":"1.5s"});
-                $(".projects").css({"transform":"translate(200px,-200px)","transition-duration":"1.5s","transition-delay":"0.5s"});
-                   $(".aboutme").css({"transform":"translateX(350px)","transition-duration":"1.5s","transition-delay":"1s"});
-                   $(".experience").css({"transform":"translate(200px,200px)","transition-duration":"1.5s","transition-delay":"1.5s"});
-                   $(".achievements").css({"transform":"translate(-200px,200px)","transition-duration":"1.5s","transition-delay":"2s"});
-
-                   $(".education").css({"transform":"translateX(-350px)","transition-duration":"1.5s","transition-delay":"2.5s"});
-                   
-                   $(".gito").css({"transform":"translate(600px,350px)","transition-duration":"1.5s","transition-delay":"3s"});
-                   $(".linkedino").css({"transform":"translate(-700px,350px)","transition-duration":"1.5s","transition-delay":"2.5s"});
-                //    $("#triangle-bottomleft ").css({"border-bottom":"200px solid white","border-right":"200px solid transparent","trans0i0tion-duration":"1.5s","transition-delay":"1.4s"});
-                $(".circle1").css({"transform":"scale(30, 30)","transition-duration":"1.5s","transition-delay":"4.5s"});
-                $(".circle2").css({"transform":"scale(30, 30)","transition-duration":"1.5s","transition-delay":"4.5s"});
-
-                });})
-            
+          
