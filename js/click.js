c2f73007c2829a1116b645fee2b184f02e35fc4b
nikhil24agarwal <nikhil12386@yahoo.com>
2020-07-13 21:52:34 +0530

    animation upto content particular page indidualy comiited

--- js/click.js
+++ js/click.js
@@ -1,2 +1,101 @@
+$("document").ready(function(){  
+    $(".myimage").click(function(){
+            $(".aboutme").css({"transform":"translate(200px,-200px)","transition-duration":"1.5s","transition-delay":"0.5s"});
+            $(".education").css({"transform":"translateX(350px)","transition-duration":"1.5s","transition-delay":"1s"});
+            $(".skills").css({"transform":"translate(200px,200px)","transition-duration":"1.5s","transition-delay":"1.5s"});
+            $(".experience").css({"transform":"translate(-200px,200px)","transition-duration":"1.5s","transition-delay":"2s"});
+            $(".projects").css({"transform":"translateX(-350px)","transition-duration":"1.5s","transition-delay":"2.5s"});
+            $(".achievements").css({"transform":"translate(-200px,-200px)","transition-duration":"1.5s"});
+             $(".gito").css({"transform":"translate(600px,350px)","transition-duration":"1.5s","transition-delay":"3s"});
+            $(".linkedino").css({"transform":"translate(-700px,350px)","transition-duration":"1.5s","transition-delay":"2.5s"});
+         $(".circle1").css({"transform":"scale(30, 30)","transition-duration":"1.5s","transition-delay":"4.5s"});
+         $(".circle2").css({"transform":"scale(30, 30)","transition-duration":"1.5s","transition-delay":"4.5s"});
 
-          
+
+
+         
+$("#i1").click(function(){
+    image();
+    $(".aboutme").css({"transform":"translate(-700px,-230px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".education").css({"transform":"translate(-700px,-150px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".skills").css({"transform":"translate(-700px,-70px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".experience").css({"transform":"translate(-700px,10px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".projects").css({"transform":"translate(-700px,90px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".achievements").css({"transform":"translate(-700px,170px) scale(0.3,0.3)","transition-duration":"1.5s"});
+   
+   });
+   
+   
+   $("#i2").click(function(){
+    image();
+    $(".aboutme").css({"transform":"translate(-700px,-230px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".education").css({"transform":"translate(-700px,-150px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".skills").css({"transform":"translate(-700px,-70px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".experience").css({"transform":"translate(-700px,10px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".projects").css({"transform":"translate(-700px,90px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".achievements").css({"transform":"translate(-700px,170px) scale(0.3,0.3)","transition-duration":"1.5s"});
+   });
+   
+   $("#i3").click(function(){
+    image();
+    $(".aboutme").css({"transform":"translate(-700px,-230px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".education").css({"transform":"translate(-700px,-150px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".skills").css({"transform":"translate(-700px,-70px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".experience").css({"transform":"translate(-700px,10px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".projects").css({"transform":"translate(-700px,90px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".achievements").css({"transform":"translate(-700px,170px) scale(0.3,0.3)","transition-duration":"1.5s"});
+   });
+   
+   $("#i4").click(function(){
+    image();
+    $(".aboutme").css({"transform":"translate(-700px,-230px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".education").css({"transform":"translate(-700px,-150px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".skills").css({"transform":"translate(-700px,-70px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".experience").css({"transform":"translate(-700px,10px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".projects").css({"transform":"translate(-700px,90px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".achievements").css({"transform":"translate(-700px,170px) scale(0.3,0.3)","transition-duration":"1.5s"});
+   });
+   $("#i5").click(function(){
+    image();
+    $(".aboutme").css({"transform":"translate(-700px,-230px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".education").css({"transform":"translate(-700px,-150px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".skills").css({"transform":"translate(-700px,-70px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".experience").css({"transform":"translate(-700px,10px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".projects").css({"transform":"translate(-700px,90px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".achievements").css({"transform":"translate(-700px,170px) scale(0.3,0.3)","transition-duration":"1.5s"});
+   });
+   $("#i6").click(function(){
+    image();
+    $(".aboutme").css({"transform":"translate(-700px,-230px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".education").css({"transform":"translate(-700px,-150px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".skills").css({"transform":"translate(-700px,-70px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".experience").css({"transform":"translate(-700px,10px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".projects").css({"transform":"translate(-700px,90px) scale(0.3,0.3)","transition-duration":"1.5s"});
+               $(".achievements").css({"transform":"translate(-700px,170px) scale(0.3,0.3)","transition-duration":"1.5s"});
+   });
+   
+
+
+
+         });
+
+     });
+     
+             // image sizing and position after clicking
+         function image(){                        
+             $(".myimage").css({"transform":"scale(0.4,0.4) translate(-1760px,-800px)","transition-duration":"5s"});
+
+ // $(".myimage").css({"transform":"translateY(200px)"});
+
+}
+
+// function a(){
+//     $(".myimage").css({"transform":"scale(0.4,0.4) translate(-1760px,-800px)","transition-duration":"5s"});
+//     $(".aboutme").css({"transform":"translate(-700px,-230px) scale(0.3,0.3)","transition-duration":"1.5s"});
+//                $(".education").css({"transform":"translate(-700px,-150px) scale(0.3,0.3)","transition-duration":"1.5s"});
+//                $(".skills").css({"transform":"translate(-700px,-70px) scale(0.3,0.3)","transition-duration":"1.5s"});
+//                $(".experience").css({"transform":"translate(-700px,10px) scale(0.3,0.3)","transition-duration":"1.5s"});
+//                $(".projects").css({"transform":"translate(-700px,90px) scale(0.3,0.3)","transition-duration":"1.5s"});
+//                $(".achievements").css({"transform":"translate(-700px,170px) scale(0.3,0.3)","transition-duration":"1.5s"});
+// }
+
